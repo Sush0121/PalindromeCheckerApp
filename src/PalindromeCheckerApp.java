6c57cd7e58240abe4e431e17ae304a24d14f1057
Sush0121 <sm7580@srmist.edu.in>
2026-02-23 09:51:59 +0530

    Use Case-2

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,11 +1,22 @@
 public class PalindromeCheckerApp {
 
         public static void main(String[] args) {
-            System.out.println("======================================");
-            System.out.println("   Welcome to Palindrome Checker");
-            System.out.println("======================================");
-            System.out.println("Application Version: 1.0");
-            System.out.println("System started successfully.");
+
+            String input = "madam";
+            boolean isPalindrome = true;
+
+            for (int i = 0; i < input.length() / 2; i++) {
+                if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+
+            if (isPalindrome) {
+                System.out.println(input + " is a Palindrome");
+            } else {
+                System.out.println(input + " is not a Palindrome");
+            }
         }
 
 }
